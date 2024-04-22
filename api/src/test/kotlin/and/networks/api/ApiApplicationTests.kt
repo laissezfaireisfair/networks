d7154f14fe68b00dfa0134bb95a2ee8eca5347a7
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-04-22 18:46:25 +0700

    Add empty API Spring Boot project

---
+++ api/src/test/kotlin/and/networks/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package and.networks.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
