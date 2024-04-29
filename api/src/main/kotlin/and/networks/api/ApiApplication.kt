7c1df659ff31b73e9596396d70578afc4d1baa7e
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-04-29 19:17:18 +0700

    Add mock example web method

--- api/src/main/kotlin/and/networks/api/ApiApplication.kt
+++ api/src/main/kotlin/and/networks/api/ApiApplication.kt
@@ -2,10 +2,18 @@ package and.networks.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class ApiApplication
 
 fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
+}
+
+@RestController
+class MatchesController {
+    @GetMapping("/")
+    fun getMatches() = listOf("Match1", "Match2")
 }
